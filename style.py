1254d6e840f25c51cd0af36d6b30dc2ef3345701
yanz <dirtysalt1987@gmail.com>
2016-11-18 17:30:35 +0800

    fix output image

--- style.py
+++ style.py
@@ -157,7 +157,7 @@ def main():
                 evaluate.ffwd_to_img(options.test,preds_path,
                                      options.checkpoint_dir)
             else:
-                save_img(preds_path, img)
+                save_img(preds_path, preds[0])
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint-dir %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
