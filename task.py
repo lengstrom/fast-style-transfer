0372d9fb36f209379c8cc6b98433aad609897102
Feilong <feilong.dragona@gmail.com>
2017-01-29 00:12:30 +0800

    Adds task.py to automate the processing of multiple images

---
+++ task.py
@@ -0,0 +1,34 @@
+import evaluate
+import glob
+import os
+
+'''
+By Feilong
+Copyright (C) 2017 FeiLong"
+This is a small conttribution to the porject 
+task.py helps in processing multiple images. You just need 
+to place all your images inside the images folder and create
+a done folder for storing the generated images.
+'''
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+ckpt_names =  glob.glob(dir_path+"/*.ckpt")
+image_names = glob.glob(dir_path+"/images/*.*")
+
+print "Starting"
+for image_path in image_names:
+	temp = image_path.split('/')
+	in_path = temp[len(temp)-1]
+	
+	for file in ckpt_names:
+		temp2 = file.split('/')
+		checkpoint_dir = temp2[len(temp2)-1]
+		suffix = checkpoint_dir.split('.')
+		out_path= suffix[0]+"_"+in_path
+		print in_path, out_path
+		evaluate.ffwd_to_img("./images/"+in_path, "./done/art_"+out_path, "./"+checkpoint_dir)	
+
+print "Completed!"
+
+
+	
