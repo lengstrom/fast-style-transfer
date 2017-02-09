fdc3f6974851d630da16c775bc498997d74b2e35
Fergal Cotter <fbcotter90@gmail.com>
2017-02-09 12:49:28 +0000

    Made python3 friendly
    Replaced calls to reduce with calls to functools.reduce()
    Replaced map() function with list comprehension
    Added shebang to setup.sh file so it can be run as "./setup.sh"

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 mkdir data
 cd data
 wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
--- style.py
+++ style.py
@@ -106,7 +106,8 @@ def check_opts(opts):
 
 def _get_files(img_dir):
     files = list_files(img_dir)
-    return map(lambda x: os.path.join(img_dir,x), files)
+    return [os.path.join(img_dir,x) for x in files]
+
     
 def main():
     parser = build_parser()
