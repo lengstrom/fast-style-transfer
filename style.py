edd8d84f29ab931c59bde7b2b2d67cd955fdf8e2
Mathias Sundholm <mathias.sundholm@gmail.com>
2017-04-04 10:45:01 +0200

    fixed undefined variable img when training using option --slow

--- style.py
+++ style.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import sys, os, pdb
 sys.path.insert(0, 'src')
-import numpy as np, scipy.misc 
+import numpy as np, scipy.misc
 from optimize import optimize
 from argparse import ArgumentParser
 from utils import save_img, get_img, exists, list_files
@@ -69,7 +69,7 @@ def build_parser():
                         dest='content_weight',
                         help='content weight (default %(default)s)',
                         metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
-    
+
     parser.add_argument('--style-weight', type=float,
                         dest='style_weight',
                         help='style weight (default %(default)s)',
@@ -79,7 +79,7 @@ def build_parser():
                         dest='tv_weight',
                         help='total variation regularization weight (default %(default)s)',
                         metavar='TV_WEIGHT', default=TV_WEIGHT)
-    
+
     parser.add_argument('--learning-rate', type=float,
                         dest='learning_rate',
                         help='learning rate (default %(default)s)',
@@ -107,7 +107,7 @@ def check_opts(opts):
 def _get_files(img_dir):
     files = list_files(img_dir)
     return map(lambda x: os.path.join(img_dir,x), files)
-    
+
 def main():
     parser = build_parser()
     options = parser.parse_args()
@@ -157,7 +157,7 @@ def main():
                 evaluate.ffwd_to_img(options.test,preds_path,
                                      options.checkpoint_dir)
             else:
-                save_img(preds_path, img)
+                save_img(preds_path, pred[0,:,:,:])
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint-dir %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
