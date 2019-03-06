16f56713419e632f9aa24be47647562e015c6e93
Jess Edmund Fan <jessefan2002@gmail.com>
2019-03-05 21:20:26 -0800

    Update optimize.py

--- src/optimize.py
+++ src/optimize.py
@@ -5,7 +5,10 @@ import tensorflow as tf, numpy as np, os
 import transform
 from utils import get_img
 
-STYLE_LAYERS = ('conv1_2', 'conv2_2', 'conv3_3', 'conv4_1', 'conv5_1')
+#STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+#CONTENT_LAYER = 'conv3_1'
+
+STYLE_LAYERS = ('conv3_3')
 CONTENT_LAYER = 'conv3_2'
 DEVICES = 'CUDA_VISIBLE_DEVICES'
 
