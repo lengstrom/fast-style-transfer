710047e051239ed33a4ca08b35a27fed4df74af1
Jess Edmund Fan <jessefan2002@gmail.com>
2019-03-08 19:46:57 -0800

    Update vgg.py

--- src/vgg.py
+++ src/vgg.py
@@ -9,18 +9,16 @@ MEAN_PIXEL = np.array([ 123.68 ,  116.779,  103.939])
 
 def net(data_path, input_image):
     layers = (
-        'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
-
         'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
 
         'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
-        'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+        'relu3_3', 'pool3',
 
         'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
-        'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+        'relu4_3', 'pool4',
 
         'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
-        'relu5_3', 'conv5_4', 'relu5_4'
+        'relu5_3'
     )
 
     data = scipy.io.loadmat(data_path)
