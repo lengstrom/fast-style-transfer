9f8865f01ae23aaf766bf29a2eb5c8532f0d11f0
Jess Edmund Fan <jessefan2002@gmail.com>
2019-03-12 15:18:08 -0700

    Update vgg.py

--- src/vgg.py
+++ src/vgg.py
@@ -5,7 +5,8 @@ import numpy as np
 import scipy.io
 import pdb
 
-MEAN_PIXEL = np.array([ 123.68 ,  116.779,  103.939])
+#MEAN_PIXEL = np.array([ 123.68 ,  116.779,  103.939])
+MEAN_PIXEL = np.array([123, 117, 104])
 
 def net(data_path, input_image):
     layers = (
