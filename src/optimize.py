91b6bd2b92f8ecdac936b25191511c5ed69706d0
Jess Edmund Fan <jessefan2002@gmail.com>
2019-03-13 19:34:13 -0700

    Update optimize.py

--- src/optimize.py
+++ src/optimize.py
@@ -5,9 +5,9 @@ import tensorflow as tf, numpy as np, os
 import transform
 from utils import get_img
 
-#STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
 #CONTENT_LAYER = 'relu4_2'
-STYLE_LAYERS = ('conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1')
+#STYLE_LAYERS = ('conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1')
 CONTENT_LAYER = 'conv4_2'
 DEVICES = 'CUDA_VISIBLE_DEVICES'
 
@@ -16,16 +16,11 @@ def optimize(content_targets, style_target, content_weight, style_weight,
              tv_weight, vgg_path, epochs=2, print_iterations=1000,
              batch_size=4, save_path='saver/fns.ckpt', slow=False,
              learning_rate=1e-3, debug=False):
-    if slow:
-        batch_size = 1
-    mod = len(content_targets) % batch_size
-    if mod > 0:
-        print("Train set has been trimmed slightly..")
-        content_targets = content_targets[:-mod] 
 
     style_features = {}
 
-    batch_shape = (batch_size,256,256,3)
+    #batch_shape = (batch_size,256,256,3)
+    batch_shape = (batch_size,512,512,3)
     style_shape = (1,) + style_target.shape
     print(style_shape)
 
@@ -89,7 +84,8 @@ def optimize(content_targets, style_target, content_weight, style_weight,
 
         loss = content_loss + style_loss + tv_loss
 
-        # overall loss
+        # overall loss      
+        
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
         sess.run(tf.global_variables_initializer())
         import random
