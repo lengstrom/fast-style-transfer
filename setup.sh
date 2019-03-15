2964507b85f98130647d2c77fcdc895ae6ec463f
Jess Edmund Fan <jessefan2002@gmail.com>
2019-03-14 17:34:17 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 mkdir data
 cd data
-wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat -q --show-progress
 mkdir bin
-wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
-unzip train2014.zip
+wget http://images.cocodataset.org/zips/train2017.zip -q --show-progress
+unzip train2017.zip
