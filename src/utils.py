debd151faf1ffd4330c7849e097568ba8fc06d5b
Javier <jahnog@gmail.com>
2020-07-02 14:03:40 -0300

    Code cleanup for python 3.

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,36 @@
-import scipy.misc, numpy as np, os, sys
+import os
+
 import imageio
+import numpy as np
 from PIL import Image
+
 
 def save_img(out_path, img):
     img = np.clip(img, 0, 255).astype(np.uint8)
     imageio.imwrite(out_path, img)
+
 
 def scale_img(style_path, style_scale):
     scale = float(style_scale)
     o0, o1, o2 = imageio.imread(style_path, pilmode='RGB').shape
     scale = float(style_scale)
     new_shape = (int(o0 * scale), int(o1 * scale), o2)
-    style_target = _get_img(style_path, img_size=new_shape)
+    style_target = get_img(style_path, img_size=new_shape)
     return style_target
 
-def get_img(src, img_size=False):
-   img = imageio.imread(src, pilmode='RGB') # misc.imresize(, (256, 256, 3))
-   if not (len(img.shape) == 3 and img.shape[2] == 3):
-       img = np.dstack((img,img,img))
-   if img_size != False:
-       img = np.array(Image.fromarray(img).resize(img_size[:2]))
-   return img
 
-def exists(p, msg):
+def get_img(src, img_size=False):
+    img = imageio.imread(src, pilmode='RGB')  # misc.imresize(, (256, 256, 3))
+    if not (len(img.shape) == 3 and img.shape[2] == 3):
+        img = np.dstack((img, img, img))
+    if img_size is not False:
+        img = np.array(Image.fromarray(img).resize(img_size[:2]))
+    return img
+
+
+def exists(p, msg: str = ''):
     assert os.path.exists(p), msg
+
 
 def list_files(in_path):
     files = []
--- style.py
+++ style.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
-import sys, os, pdb
-sys.path.insert(0, 'src')
-import numpy as np, scipy.misc 
-from optimize import optimize
+
+import os
 from argparse import ArgumentParser
-from utils import save_img, get_img, exists, list_files
+
 import evaluate
+from src.optimize import optimize
+from src.utils import save_img, get_img, exists, list_files
 
 CONTENT_WEIGHT = 7.5e0
 STYLE_WEIGHT = 1e2
@@ -20,6 +20,7 @@ TRAIN_PATH = 'data/train2014'
 BATCH_SIZE = 4
 DEVICE = '/gpu:0'
 FRAC_GPU = 1
+
 
 def build_parser():
     parser = ArgumentParser()
@@ -69,7 +70,7 @@ def build_parser():
                         dest='content_weight',
                         help='content weight (default %(default)s)',
                         metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
-    
+
     parser.add_argument('--style-weight', type=float,
                         dest='style_weight',
                         help='style weight (default %(default)s)',
@@ -79,13 +80,14 @@ def build_parser():
                         dest='tv_weight',
                         help='total variation regularization weight (default %(default)s)',
                         metavar='TV_WEIGHT', default=TV_WEIGHT)
-    
+
     parser.add_argument('--learning-rate', type=float,
                         dest='learning_rate',
                         help='learning rate (default %(default)s)',
                         metavar='LEARNING_RATE', default=LEARNING_RATE)
 
     return parser
+
 
 def check_opts(opts):
     exists(opts.checkpoint_dir, "checkpoint dir not found!")
@@ -104,11 +106,12 @@ def check_opts(opts):
     assert opts.tv_weight >= 0
     assert opts.learning_rate >= 0
 
+
 def _get_files(img_dir):
     files = list_files(img_dir)
-    return [os.path.join(img_dir,x) for x in files]
+    return [os.path.join(img_dir, x) for x in files]
 
-    
+
 def main():
     parser = build_parser()
     options = parser.parse_args()
@@ -121,12 +124,12 @@ def main():
         content_targets = [options.test]
 
     kwargs = {
-        "slow":options.slow,
-        "epochs":options.epochs,
-        "print_iterations":options.checkpoint_iterations,
-        "batch_size":options.batch_size,
-        "save_path":os.path.join(options.checkpoint_dir,'fns.ckpt'),
-        "learning_rate":options.learning_rate
+        "slow": options.slow,
+        "epochs": options.epochs,
+        "print_iterations": options.checkpoint_iterations,
+        "batch_size": options.batch_size,
+        "save_path": os.path.join(options.checkpoint_dir, 'fns.ckpt'),
+        "learning_rate": options.learning_rate
     }
 
     if options.slow:
@@ -151,17 +154,18 @@ def main():
         to_print = (style_loss, content_loss, tv_loss)
         print('style: %s, content:%s, tv: %s' % to_print)
         if options.test:
-            assert options.test_dir != False
-            preds_path = '%s/%s_%s.png' % (options.test_dir,epoch,i)
+            assert options.test_dir is not False
+            preds_path = '%s/%s_%s.png' % (options.test_dir, epoch, i)
             if not options.slow:
                 ckpt_dir = os.path.dirname(options.checkpoint_dir)
-                evaluate.ffwd_to_img(options.test,preds_path,
+                evaluate.ffwd_to_img(options.test, preds_path,
                                      options.checkpoint_dir)
             else:
                 save_img(preds_path, img)
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
+
 
 if __name__ == '__main__':
     main()
